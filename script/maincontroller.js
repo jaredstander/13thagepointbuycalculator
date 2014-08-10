e8cf9c0ca0e17b8a7d31fcc34ac95c31400ae2ab
jaredstander <jaredstander@gmail.com>
2014-08-10 01:39:36 -0400

    Refactored code to less lines and less functions.

--- script/maincontroller.js
+++ script/maincontroller.js
@@ -1,31 +1,31 @@
 app.controller('MainController', function($scope) {
   $scope.pointsLeft = 28;
   $scope.abilityLabels = new Array("STR: ", "DEX: ", "CON: ", "INT: ", "WIS: ", "CHA: ");
-  $scope.modLabel = " | Mod: "
+  $scope.modLabel = " Mod: "
   $scope.pointBuyScore = new Array(8, 8, 8, 8, 8, 8);
   $scope.racialBonus = new Array(0, 0, 0, 0, 0, 0);
   $scope.classBonus = new Array(0, 0, 0, 0, 0, 0);
   $scope.abilityMod = new Array(-1, -1, -1, -1, -1, -1);
-  $scope.racialStats = [
-                        {id: -1, label: "None"},
-                        {id: 0, label: "Strength"},
-                        {id: 1, label: "Dexterity"},
-                        {id: 2, label: "Constitution"},
-                        {id: 3, label: "Intelligence"},
-                        {id: 4, label: "Wisdom"},
-                        {id: 5, label: "Charisma"}];
-  $scope.classStats = [
-                        {id: -1, label: "None"},
-                        {id: 0, label: "Strength"},
-                        {id: 1, label: "Dexterity"},
-                        {id: 2, label: "Constitution"},
-                        {id: 3, label: "Intelligence"},
-                        {id: 4, label: "Wisdom"},
-                        {id: 5, label: "Charisma"}];
+  $scope.statLabels = new Array("Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma");
+  $scope.racialStats = [{id: -1, label: "None", value: 0},
+                        {id: 0, label: "Strength", value: 2},
+                        {id: 1, label: "Dexterity", value: 2},
+                        {id: 2, label: "Constitution", value: 2},
+                        {id: 3, label: "Intelligence", value: 2},
+                        {id: 4, label: "Wisdom", value: 2},
+                        {id: 5, label: "Charisma", value: 2}];
+  $scope.classStats = [ {id: -1, label: "None", value: 0},
+                        {id: 0, label: "Strength", value: 2},
+                        {id: 1, label: "Dexterity", value: 2},
+                        {id: 2, label: "Constitution", value: 2},
+                        {id: 3, label: "Intelligence", value: 2},
+                        {id: 4, label: "Wisdom", value: 2},
+                        {id: 5, label: "Charisma", value: 2}];
   $scope.racialAbilityScoreSelected = $scope.racialStats[0];
   $scope.classAbilityScoreSelected = $scope.classStats[0];
 
   $scope.getAbilityMod = function(abilityScore){ return parseInt(Math.floor((abilityScore / 2) - 5)) };
+  $scope.setAbilityMod = function(index){ $scope.abilityMod[index] = parseInt($scope.getAbilityMod($scope.pointBuyScore[index] + $scope.racialBonus[index] + $scope.classBonus[index]))};
 
   $scope.getPointValue = function(abilityScore){
     // return (parseInt(abilityScore) - 8) Does not work starting at 15, will investigate another formula
@@ -67,14 +67,6 @@ app.controller('MainController', function($scope) {
     }
   };
 
-  $scope.setAbilityMod = function(index){ $scope.abilityMod[index] = parseInt($scope.getAbilityMod($scope.pointBuyScore[index] + $scope.racialBonus[index] + $scope.classBonus[index]))};
-
-  $scope.updateAbilityMods = function(){
-    for (var i = 0; i < 6; i++){
-      $scope.abilityMod[i] = parseInt($scope.getAbilityMod($scope.pointBuyScore[i] + $scope.racialBonus[i] + $scope.classBonus[i]));
-    }
-  };
-
   $scope.incrementScore = function(attribute, index){
     var abilityScore = parseInt(attribute);
     if(attribute < 18) {
@@ -97,103 +89,30 @@ app.controller('MainController', function($scope) {
     }
   };
 
-  $scope.selectRaceBonus = function(attribute, optionIndex){
-    if(attribute != "None"){
-      // remove all racial bonuses for all attributes
-      for (var i = 0; i < 6; i++){
-        $scope.racialBonus[i] = 0;
-      }
-      // add new racial bonus
-      $scope.racialBonus[optionIndex] = 2;
-      $scope.updateAbilityMods();
-      // search the classBonus array for a missing attribute
+  // Race and class bonus cannot be the same.
+  // Therefore, we remove the ability from the opposite list of options.
+  // Likewise, restore abilities that are deselected by the mechanic of choosing a new one.
+  $scope.selectRacialBonus = function(attribute, optionId){
+    if($scope.classStats.length < 7){
       for (var i = 0; i < 7; i++){
-        if(!(i in $scope.classStats) || $scope.classStats[i].id != (i - 1)){
-          // insert the missing attribute into the options array once found
-          $scope.classStats.splice(i, 0, {id: (i - 1), label: $scope.getAttributeById(i - 1)});
-        }
-      }
-      // remove the now racially-selected stat from the class options array
-      // we have to do this AFTER inserting the missing one because when the...
-      // ...options array is complete we know with certainty what stats are at which position.
-      $scope.classStats.splice(optionIndex + 1, 1);
-    } else if(attribute == "None"){
-      // remove all racial bonuses for all attributes
-      for (var i = 0; i < 6; i++){
-        $scope.racialBonus[i] = 0;
-      }
-      $scope.updateAbilityMods();
-      // search the classBonus array for a missing attribute
-      for (var i = 0; i < 7; i++){
-        if(!(i in $scope.classStats) || $scope.classStats[i].id != (i - 1)){
-          // insert the missing attribute into the options array once found
-          // we are not removing an attribute options since the user has selected no racial bonus
-          $scope.classStats.splice(i, 0, {id: (i - 1), label: $scope.getAttributeById(i - 1)});
-        }
+        if(!(i in $scope.classStats) || $scope.classStats[i].id != (i - 1)){$scope.classStats.splice(i, 0, {id: (i - 1), label: $scope.statLabels[i - 1], value: 2});      }
       }
     }
+    if(attribute !== "None"){$scope.classStats.splice(optionId + 1, 1);}
+    if($scope.racialBonus.indexOf(2) >= 0){$scope.racialBonus[$scope.racialBonus.indexOf(2)] = 0}
+    $scope.racialBonus[optionId] = $scope.racialAbilityScoreSelected.value;
+    $scope.setAbilityMod(optionId);
   };
 
-  $scope.selectClassBonus = function(attribute, optionIndex){
-    if(attribute != "None"){
-      // remove all class bonuses for all attributes
-      for (var i = 0; i < 6; i++){
-        $scope.classBonus[i] = 0;
-      }
-      // add new class bonus
-      $scope.classBonus[optionIndex] = 2;
-      $scope.updateAbilityMods();
-      // search the racialBonus array for a missing attribute
+  $scope.selectClassBonus = function(attribute, optionId){
+    if($scope.racialStats.length < 7){
       for (var i = 0; i < 7; i++){
-        if(!(i in $scope.racialStats) || $scope.racialStats[i].id != (i - 1)){
-          // insert the missing attribute into the options array once found
-          $scope.racialStats.splice(i, 0, {id: (i - 1), label: $scope.getAttributeById(i - 1)});
-        }
-      }
-      // remove the now class-selected stat from the racial options array
-      // we have to do this AFTER inserting the missing one because when the...
-      // ...options array is complete we know with certainty what stats are at which position.
-      $scope.racialStats.splice(optionIndex + 1, 1);
-    } else if(attribute == "None"){
-      // remove all class bonuses for all attributes
-      for (var i = 0; i < 6; i++){
-        $scope.classBonus[i] = 0;
-      }
-      $scope.updateAbilityMods();
-      // search the racialBonus array for a missing attribute
-      for (var i = 0; i < 7; i++){
-        if(!(i in $scope.racialStats) || $scope.racialStats[i].id != (i - 1)){
-          // insert the missing attribute into the options array once found
-          // we are not removing an attribute options since the user has selected no class bonus
-          $scope.racialStats.splice(i, 0, {id: (i - 1), label: $scope.getAttributeById(i - 1)});
-        }
+        if(!(i in $scope.racialStats) || $scope.racialStats[i].id != (i - 1)){$scope.racialStats.splice(i, 0, {id: (i - 1), label: $scope.statLabels[i - 1], value: 2});      }
       }
     }
-  };
-
-  $scope.getAttributeById = function(id){
-    switch(id){
-      case -1:
-        return "None";
-        break;
-      case 0:
-        return "Strength";
-        break;
-      case 1:
-        return "Dexterity";
-        break;
-      case 2:
-        return "Constitution";
-        break;
-      case 3:
-        return "Intelligence";
-        break;
-      case 4:
-        return "Wisdom";
-        break;
-      case 5:
-        return "Charisma";
-        break;
-    }
+    if(attribute !== "None"){$scope.racialStats.splice(optionId + 1, 1);}
+    if($scope.classBonus.indexOf(2) >= 0){$scope.classBonus[$scope.classBonus.indexOf(2)] = 0}
+    $scope.classBonus[optionId] = $scope.classAbilityScoreSelected.value;
+    $scope.setAbilityMod(optionId);
   };
 });
